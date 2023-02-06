c9f8bf713e966b8072c6b99fffd0ebd1a570c057
eason.lee[李宜丞] <eason.lee@104.com.tw>
2023-02-06 16:58:19 +0800

    bug fix

--- controllers/ProductController.go
+++ controllers/ProductController.go
@@ -1,17 +1,13 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ProductManage(c *gin.Context) {
-	test := c.MustGet("Test").(string)
-	fmt.Printf("test: %v\n", test)
 	c.HTML(http.StatusOK, "product", gin.H{
 		"active": "product",
-		"hello":  test,
 	})
 }
