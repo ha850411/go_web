c4db8ca986373b1da4649bc335d1e51ac2b7439f
eason.lee[李宜丞] <eason.lee@104.com.tw>
2023-02-15 11:41:39 +0800

    log 設定

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"goWeb/conf"
 	"goWeb/routes"
+	"io"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+/*
+* 載入 log 設定
+ */
+func init() {
+	var logDir = "./logs"
+	_, err := os.Stat(logDir)
+	if err != nil {
+		os.Mkdir(logDir, os.ModePerm)
+	}
+	file, _ := os.Create(fmt.Sprintf("./logs/%s.log", time.Now().Format("2006-01-02")))
+	gin.DefaultWriter = io.MultiWriter(file, os.Stdout)
+}
 
 func main() {
 	// 載入設定檔
