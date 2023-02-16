35c0dae7129892c68be723c502974d157019c0b9
eason.lee[李宜丞] <eason.lee@104.com.tw>
2023-02-16 11:17:42 +0800

    fix

--- service/file.go
+++ service/file.go
@@ -17,5 +17,5 @@ func WriteFileAndCompress(targetDir string, fileName string, content []byte) {
 	f, _ := os.OpenFile(targetDir+"/"+fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, os.ModePerm)
 	defer f.Close()
 	f.Write(content)
-	exec.Command("convert", "-quality", "80", targetDir+"/"+fileName, targetDir+"/"+fileName).Run()
+	exec.Command("convert", targetDir+"/"+fileName, "-quality", "80", targetDir+"/"+fileName).Run()
 }
