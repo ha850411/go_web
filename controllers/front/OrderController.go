b216f95733143159c092f590167e7ea7a803b180
eason.lee[李宜丞] <eason.lee@104.com.tw>
2023-03-08 16:24:50 +0800

    do somethings

--- controllers/front/OrderController.go
+++ controllers/front/OrderController.go
@@ -7,7 +7,6 @@ import (
 	"goWeb/service/linebot"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,14 +16,20 @@ func Orders(c *gin.Context) {
 }
 
 func OrdersAdd(c *gin.Context) {
-	postData := map[string]string{
-		"name":    c.PostForm("name"),
-		"contact": c.PostForm("contact"),
-		"address": c.PostForm("address"),
-		"remark":  c.PostForm("remark"),
-		"detail":  c.PostForm("detail"),
+	type PostData struct {
+		Name    string `json:"name"`
+		Contact string `json:"contact"`
+		Address string `json:"address"`
+		Remark  string `json:"remark"`
+		Detail  string `json:"detail"`
 	}
-	sql := fmt.Sprintf("INSERT INTO orders (name, contact, address, remark) VALUES ('%s', '%s','%s', '%s')", postData["name"], postData["contact"], postData["address"], postData["remark"])
+	postData := PostData{}
+	rawData, _ := c.GetRawData()
+	json.Unmarshal(rawData, &postData)
+	fmt.Printf("postData: %v\n", postData)
+
+	sql := fmt.Sprintf("INSERT INTO orders (name, contact, address, remark) VALUES ('%s', '%s','%s', '%s')", postData.Name, postData.Contact, postData.Address, postData.Remark)
+	fmt.Printf("sql: %v\n", sql)
 	db := database.DbConnect()
 	rows, err := db.Exec(sql)
 	if err != nil {
@@ -37,31 +42,35 @@ func OrdersAdd(c *gin.Context) {
 	if rowsAffected, _ := rows.RowsAffected(); rowsAffected > 0 {
 		// 解析 json 並新增訂單內容
 		var detail map[string]map[string]interface{}
-		_ = json.Unmarshal([]byte(postData["detail"]), &detail)
+		_ = json.Unmarshal([]byte(postData.Detail), &detail)
 		LastInsertId, _ := rows.LastInsertId()
-		lineMessage := fmt.Sprintf("\n*新訂單\n姓名: %s\n聯絡方式: %s\n配送地址: %s\n備註: %s\n訂購商品:", postData["name"], postData["contact"], postData["address"], postData["remark"])
+		lineMessage := fmt.Sprintf("\n*新訂單\n姓名: %s\n聯絡方式: %s\n配送地址: %s\n備註: %s\n訂購商品:", postData.Name, postData.Contact, postData.Address, postData.Remark)
 		var total int
 		for _, jsonMap := range detail {
 			// get price
 			var price int
-			amount, _ := strconv.Atoi(jsonMap["amount"].(string))
-			db.QueryRow(`SELECT price FROM products WHERE id= ?`, jsonMap["pid"]).Scan(&price)
+			amount := int(jsonMap["amount"].(float64))
+			db.QueryRow(`SELECT price FROM products WHERE id= ?`, jsonMap["id"]).Scan(&price)
 			// do insert
-			sql := fmt.Sprintf("INSERT INTO orders_detail (order_id, pid, amount, price, total) VALUES (%v, %v, %v, %v, %v)", LastInsertId, jsonMap["pid"], jsonMap["amount"], price, price*amount)
+			sql := fmt.Sprintf("INSERT INTO orders_detail (order_id, pid, amount, price, total) VALUES (%v, %v, %v, %v, %v)", LastInsertId, jsonMap["id"], jsonMap["amount"], price, price*amount)
 			db.Exec(sql)
 			// 推播訊息
-			sql = fmt.Sprintf("SELECT name FROM products WHERE id = %s", jsonMap["pid"])
+			sql = fmt.Sprintf("SELECT name FROM products WHERE id = %v", jsonMap["id"])
 			var pname string
 			db.QueryRow(sql).Scan(&pname)
-			lineMessage += fmt.Sprintf("\n  商品: %s\n  數量: %s\n  單價: %v\n", pname, jsonMap["amount"], price)
+			lineMessage += fmt.Sprintf("\n  商品: %s\n  數量: %v\n  單價: %v\n", pname, amount, price)
 			// 計算總價
 			total += price * amount
 		}
 		lineMessage += fmt.Sprintf("===============\n總價: %v", total)
 		// 推播
 		linebot.Request(lineMessage)
-		c.Header("Content-Type", "text/html; charset=utf-8")
-		c.String(200, `<script>alert('訂購成功');location='/orders'</script>`)
+		c.JSON(http.StatusOK, gin.H{
+			"code": http.StatusOK,
+			"msg":  "新增成功",
+		})
+		// c.Header("Content-Type", "text/html; charset=utf-8")
+		// c.String(200, `<script>alert('訂購成功');location='/orders'</script>`)
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"code": http.StatusBadRequest,
