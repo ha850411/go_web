33958636d7f7ba3fa7dda656f5f4e50eae1dc505
eason.lee[李宜丞] <eason.lee@104.com.tw>
2023-03-14 16:44:20 +0800

    檔案 > 5MB 才執行壓縮

--- service/file.go
+++ service/file.go
@@ -30,19 +30,28 @@ func WriteFileAndCompress(targetDir string, fileName string, content []byte) {
 	f, _ := os.OpenFile(targetDir+"/"+fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, os.ModePerm)
 	defer f.Close()
 	f.Write(content)
-	contentType := http.DetectContentType(content)
-	fmt.Printf("contentType: %v\n", contentType)
-	if contentType == "image/jpeg" {
-		returnData, err := exec.Command("convert", "-sample", "50%x50%", targetDir+"/"+fileName, targetDir+"/"+fileName).CombinedOutput()
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
+	fi, err := os.Stat(targetDir + "/" + fileName)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+	size := fi.Size()
+	compressSize := 1024 * 1024 * 5 // 5MB 以上要壓縮
+	if int(size) > compressSize {
+		fmt.Println("檔案 > 5MB, 執行壓縮")
+		contentType := http.DetectContentType(content)
+		fmt.Printf("contentType: %v\n", contentType)
+		if contentType == "image/jpeg" {
+			returnData, err := exec.Command("convert", "-sample", "50%x50%", targetDir+"/"+fileName, targetDir+"/"+fileName).CombinedOutput()
+			if err != nil {
+				fmt.Printf("err: %v\n", err)
+			}
+			fmt.Printf("returnData: %v\n", string(returnData))
+		} else {
+			returnData, err := exec.Command("convert", targetDir+"/"+fileName, "-quality", "80", targetDir+"/"+fileName).CombinedOutput()
+			if err != nil {
+				fmt.Printf("err: %v\n", err)
+			}
+			fmt.Printf("returnData: %v\n", string(returnData))
 		}
-		fmt.Printf("returnData: %v\n", string(returnData))
-	} else {
-		returnData, err := exec.Command("convert", targetDir+"/"+fileName, "-quality", "80", targetDir+"/"+fileName).CombinedOutput()
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
-		}
-		fmt.Printf("returnData: %v\n", string(returnData))
 	}
 }
