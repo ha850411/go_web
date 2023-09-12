8adddd400e84d3d3884cf916a112c27924448e49
eason.lee[李宜丞] <eason.lee@104.com.tw>
2023-09-12 15:07:46 +0800

    fix bug

--- log.sh
+++ log.sh
@@ -10,11 +10,5 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 sourceFile=$DIR/logs/run.log
 targetFile=$DIR/logs/log_$(date '+%Y%m%d').log
 
-current_month=$(date +"%Y%m")
-preMonthFile=$DIR/logs/log_$(date -v -1m -j -f "%Y%m" "$current_month" +"%Y%m")*
-
 cp $sourceFile $targetFile
 cat /dev/null > $sourceFile
-
-# 刪除前一個月 log
-rm -rf $preMonthFile
