eab1873b9bdcb62c6ec35cb4eaed15bd373a354f
eason.lee[李宜丞] <eason.lee@104.com.tw>
2024-01-30 14:54:44 +0800

    更換 domain

--- autocert.sh
+++ autocert.sh
@@ -2,17 +2,18 @@
 
 #go project path
 projectPath=/home/gopath/go_web
+myHost=easonlee.shop
 
 #備份原來的憑證
 mv $projectPath/certs/fullchain.crt $projectPath/certs/fullchain.crt.bk.$(date '+%Y%m%d')
 mv $projectPath/certs/privkey.key $projectPath/certs/privkey.key.bk.$(date '+%Y%m%d')
 
 #產生憑證
-certbot certonly --webroot -w /home/gopath/go_web -d easonweb.site --email le850411@gmail.com
+certbot certonly --webroot -w /home/gopath/go_web -d $myHost --email le850411@gmail.com
 
 #複製憑證
-cp /etc/letsencrypt/live/easonweb.site/fullchain.pem $projectPath/certs/fullchain.crt
-cp /etc/letsencrypt/live/easonweb.site/privkey.pem $projectPath/certs/privkey.key
+cp /etc/letsencrypt/live/$myHost/fullchain.pem $projectPath/certs/fullchain.crt
+cp /etc/letsencrypt/live/$myHost/privkey.pem $projectPath/certs/privkey.key
 
 #重啟 docker 重新掛載證書)
 docker-compose restart
